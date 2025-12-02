7f8c89194fbc7d5c714bc7c5e071b8208681cd89
Niklas Krieger <niklask.coding@gmail.com>
2025-12-02 04:40:41 +0100

    workspaces setup & initial dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,35 @@
+import tseslint from "typescript-eslint";
+import prettier from "eslint-plugin-prettier";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import eslint from "@eslint/js";
+
+export default tseslint.config(
+    {
+        ignores: ["packages/*/lib/"]
+    },
+    eslint.configs.recommended,
+    prettierRecommended,
+    ...tseslint.configs.recommended,
+    {
+        plugins: {
+            prettier
+        },
+        rules: {
+            "prettier/prettier": "error",
+            "no-console": "error",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@typescript-eslint/no-non-null-assertion": "off",
+            "@typescript-eslint/no-namespace": "off",
+            "@typescript-eslint/consistent-type-imports": "error",
+
+            "@typescript-eslint/no-unused-vars": [
+                "error",
+                {
+                    argsIgnorePattern: "^_"
+                }
+            ],
+
+            "no-async-promise-executor": "off"
+        }
+    }
+);
