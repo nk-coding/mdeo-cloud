6f92725ffdb73181933946d81a2979a613e19145
Niklas Krieger <niklask.coding@gmail.com>
2026-02-13 23:19:06 +0100

    progress

--- app/packages/editor-model/src/model/elementTypes.ts
+++ app/packages/editor-model/src/model/elementTypes.ts
@@ -12,10 +12,6 @@ export enum ModelElementType {
      */
     LABEL_OBJECT_NAME = "label:object-name",
     /**
-     * @deprecated Use LABEL_OBJECT_NAME which now displays both name and type
-     */
-    LABEL_OBJECT_TYPE = "label:object-type",
-    /**
      * Label displaying a property assignment
      */
     LABEL_PROPERTY = "label:property",
@@ -39,20 +35,4 @@ export enum ModelElementType {
      * Label at the end of a link (source or target)
      */
     LABEL_LINK_END = "label:link-end",
-    /**
-     * @deprecated Use NODE_LINK_END with end property
-     */
-    NODE_LINK_SOURCE = "node:link-source",
-    /**
-     * @deprecated Use NODE_LINK_END with end property
-     */
-    NODE_LINK_TARGET = "node:link-target",
-    /**
-     * @deprecated Use LABEL_LINK_END
-     */
-    LABEL_LINK_SOURCE = "label:link-source",
-    /**
-     * @deprecated Use LABEL_LINK_END
-     */
-    LABEL_LINK_TARGET = "label:link-target"
 }
